1a0e4cc16f293ba7934fa5802605f443b0b7cab5
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 11:59:00 +0700

    Create latihan_10.java

---
+++ latihan_10.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra230922;
+
+/**
+ *
+ * @author HP
+ */
+public class latihan_10 {
+  public static void main( String[] args ){ 
+  
+  int number = 10;
+  char letter = 'a' ;
+  boolean result =true; 
+ 
+ 
+  System.out.println("Number =" + number);
+  System.out.println("Letter =" + letter);
+  System.out.println("Result =" + result);
+  System.out.println("str = hello");
+
+  }  
+}
