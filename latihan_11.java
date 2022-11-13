53c2ec1efb02318ae7fcb9339f2869c77f5bca5d
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 11:59:12 +0700

    Create latihan_11.java

---
+++ latihan_11.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra230922;
+
+/**
+ *
+ * @author HP
+ */
+public class latihan_11 {
+    public static void main( String[] args ){ 
+  
+  int number = 10;
+  int number2= 20 ;
+  int number3= 45 ;
+  int rt;
+  
+  rt= (number + number2 + number3)/3;
+  System.out.println("Number 1 =" + number);
+  System.out.println("Number 2 =" + number2);
+  System.out.println("Number 3 =" + number3);
+  System.out.println("Average =" + rt);
+  
+    }
+    
+}
